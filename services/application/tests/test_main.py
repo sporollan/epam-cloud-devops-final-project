986f816b7f26ff580ca79e90fe13be288fea0022
sporollan <santiago.porollan@gmail.com>
2025-06-12 20:21:54 -0300

    Initialize application

---
+++ services/application/tests/test_main.py
@@ -0,0 +1,8 @@
+import pytest
+
+from app.main import greeting
+
+@pytest.mark.asyncio
+async def test_greeting_function():
+    result = await greeting()
+    assert result == {"greeting": "Hello, World!"}
