5ad70bfb4d0fdb5b6737678c618b76068de6fcaa
sporollan <santiago.porollan@gmail.com>
2025-06-14 00:04:33 -0300

    the s3 bucket got initialized on us-west-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "environment/my-app/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     encrypt        = true
     dynamodb_table = "terraform-state-locks"
   }
