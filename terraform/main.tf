01fcde2e11761bb0a0b76c9fa58a702333d25386
sporollan <santiago.porollan@gmail.com>
2025-06-14 00:01:10 -0300

    Keep backend on s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,14 @@
-# main.tf
 terraform {
   required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "environment/my-app/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-locks"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
